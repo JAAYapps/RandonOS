b18c28f1610cf49a318b4f9ff6a4d456f417cdbb
Joshua Vanderzee <vanderzee24@yahoo.com>
2021-05-15 23:15:51 -0400

    Added bootloader code

---
+++ gnu-efi/bootloader/main.c
@@ -0,0 +1,226 @@
+#include <efi.h>
+#include <efilib.h>
+#include <elf.h>
+
+typedef unsigned long long size_t;
+
+typedef struct
+{
+	void* BaseAddress;
+	size_t BufferSize;
+	unsigned int Width;
+	unsigned int Height;
+	unsigned int PixelsPerScanLine;
+} FrameBuffer;
+
+#define PSF1_MAGIC0 0x36
+#define PSF1_MAGIC1 0x04
+
+typedef struct
+{
+	unsigned char magic[2];
+	unsigned char mode;
+	unsigned char charSize;
+} PSF1_HEADER;
+
+typedef struct
+{
+	PSF1_HEADER* psf1_Header;
+	void* glyphBuffer;
+} PSF1_FONT;
+
+FrameBuffer frameBuffer;
+FrameBuffer* InitializeGOP()
+{
+	EFI_GUID gopGuid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
+	EFI_GRAPHICS_OUTPUT_PROTOCOL* gop;
+	EFI_STATUS status;
+
+	status = uefi_call_wrapper(BS->LocateProtocol, 3, &gopGuid, NULL, (void**)&gop);
+	if (EFI_ERROR(status))
+	{
+		Print(L"The graphics filling is missing. :(\n\r");
+		return NULL;
+	}
+	Print(L"The graphics filling goes great with this. :)\n\r");
+
+	frameBuffer.BaseAddress = (void*)gop->Mode->FrameBufferBase;
+	frameBuffer.BufferSize = gop->Mode->FrameBufferSize;
+	frameBuffer.Width = gop->Mode->Info->HorizontalResolution;
+	frameBuffer.Height = gop->Mode->Info->VerticalResolution;
+	frameBuffer.PixelsPerScanLine = gop->Mode->Info->PixelsPerScanLine;
+
+	return &frameBuffer;
+}
+
+EFI_FILE* LoadFile(EFI_FILE* Directory, CHAR16* Path, EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
+{
+	EFI_FILE* LoadedFile;
+
+	EFI_LOADED_IMAGE_PROTOCOL* LoadedImage;
+	SystemTable->BootServices->HandleProtocol(ImageHandle, &gEfiLoadedImageProtocolGuid, (void**)& LoadedImage);
+
+	EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* FileSystem;
+
+	SystemTable->BootServices->HandleProtocol(LoadedImage->DeviceHandle, &gEfiSimpleFileSystemProtocolGuid, (void**)& FileSystem);
+
+	if (Directory == NULL)
+	{
+		FileSystem->OpenVolume(FileSystem, &Directory);
+	}
+
+	EFI_STATUS s = Directory->Open(Directory, &LoadedFile, Path, EFI_FILE_MODE_READ, EFI_FILE_READ_ONLY);
+
+	if (s != EFI_SUCCESS)
+	{
+		return NULL;
+	}
+	return LoadedFile;
+}
+
+PSF1_FONT* LoadPSF1Font(EFI_FILE* Directory, CHAR16* Path, EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
+{
+	EFI_FILE* font = LoadFile(Directory, Path, ImageHandle, SystemTable);
+	if (font == NULL) 
+	{
+		Print(L"Font is missing.");
+		return NULL;		
+	}
+
+	PSF1_HEADER* fontHeader;
+	SystemTable->BootServices->AllocatePool(EfiLoaderData, sizeof(PSF1_HEADER), (void**)&fontHeader);
+	UINTN size = sizeof(PSF1_HEADER);
+	font->Read(font, &size, fontHeader);
+
+	if (fontHeader->magic[0] != PSF1_MAGIC0 || fontHeader->magic[1] != PSF1_MAGIC1)
+	{
+		Print(L"Font has spoiled.");
+		return NULL;
+	}
+
+	UINTN glyphBufferSize = fontHeader->charSize * 256;
+	if (fontHeader->mode == 1)
+	{
+		Print(L"Font has charSize * 512.");
+		glyphBufferSize = fontHeader->charSize * 512;
+	}
+	void* glyphBuffer;
+	{
+		font->SetPosition(font, sizeof(PSF1_HEADER));
+		SystemTable->BootServices->AllocatePool(EfiLoaderData, glyphBufferSize, (void**)&glyphBuffer);
+		font->Read(font, &glyphBufferSize, glyphBuffer);
+	}
+	
+	PSF1_FONT* finishedFont;
+	SystemTable->BootServices->AllocatePool(EfiLoaderData, sizeof(PSF1_FONT), (void**)&finishedFont);
+	finishedFont->psf1_Header = fontHeader;
+	finishedFont->glyphBuffer = glyphBuffer;
+	return finishedFont;
+}
+
+int memoryCompare(const void* sourceFile, const void* comparedSourceFile, size_t n)
+{
+	const int isSmaller = -1;
+	const int isBigger = 1;
+	const int matched = 0;
+
+	const unsigned char *source = sourceFile, *comparedSource = comparedSourceFile;
+	for (size_t i = 0l; i < n; i++)
+	{
+		if (source[i] < comparedSource[i]) return isSmaller;
+		else if (source[i] > comparedSource[i]) return isBigger;
+	}
+	return matched;
+}
+
+EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+	InitializeLib(ImageHandle, SystemTable);
+
+	Print(L"Loading the Random OS that makes no sense.\n\r");
+
+	EFI_FILE* Kernel = LoadFile(NULL, L"kernel.elf", ImageHandle, SystemTable);
+	if (Kernel == NULL)
+	{
+		Print(L"Could not load the kernel :(\n\r");
+	}
+	else
+	{
+		Print(L"Kernel loaded successfully :)\n\r");
+	}
+
+	Elf64_Ehdr header;
+	{
+		UINTN FileInfoSize;
+		EFI_FILE_INFO* FileInfo;
+		Kernel->GetInfo(Kernel, &gEfiFileInfoGuid, &FileInfoSize, NULL);
+		SystemTable->BootServices->AllocatePool(EfiLoaderData, FileInfoSize, (void**)& FileInfo);
+		Kernel->GetInfo(Kernel, &gEfiFileInfoGuid, &FileInfoSize, (void**)& FileInfo);
+
+		UINTN size = sizeof(header);
+		Kernel->Read(Kernel, &size, &header);
+	}
+
+	if (memoryCompare(&header.e_ident[EI_MAG0], ELFMAG, SELFMAG) != 0 || header.e_ident[EI_CLASS] != ELFCLASS64 ||
+		header.e_ident[EI_DATA] != ELFDATA2LSB || header.e_type != ET_EXEC || header.e_machine != EM_X86_64 || 
+		header.e_version != EV_CURRENT)
+	{
+		Print(L"Kernel format tastes horrible :P\n\r");
+	}
+	else
+	{
+		Print(L"Kernel header tastes good, yum :)\n\r");
+	}
+
+	Elf64_Phdr* phdrs;
+	{
+		Kernel->SetPosition(Kernel, header.e_phoff);
+		UINTN size = header.e_phnum * header.e_phentsize;
+		SystemTable->BootServices->AllocatePool(EfiLoaderData, size, (void**)& phdrs);
+		Kernel->Read(Kernel, &size, phdrs);
+	}
+
+	for (Elf64_Phdr* phdr = phdrs; 
+		(char*)phdr < (char*)phdrs + header.e_phnum * header.e_phentsize;
+		phdr = (Elf64_Phdr*)(char*)phdr + header.e_phentsize)
+	{
+		switch (phdr->p_type)
+		{
+			case PT_LOAD:
+			{
+				int pages = (phdr->p_memsz + 0x1000 - 1) / 0x1000;
+				Elf64_Addr segment = phdr->p_paddr;
+				SystemTable->BootServices->AllocatePages(AllocateAddress, EfiLoaderData, pages, &segment);
+				Kernel->SetPosition(Kernel, phdr->p_offset);
+				UINTN size = phdr->p_filesz;
+				Kernel->Read(Kernel, &size, (void*)segment);
+				break;			
+			}
+		}
+	}
+
+	Print(L"I ate the Kernel.\n\r");
+
+	char* (*KernelStart)(FrameBuffer*, PSF1_FONT*) = ((__attribute__((sysv_abi)) char* (*)(FrameBuffer*, PSF1_FONT*) ) header.e_entry);
+
+	PSF1_FONT* newFont = LoadPSF1Font(NULL, L"zap-light16.psf", ImageHandle, SystemTable);
+	if (newFont != NULL)
+	{
+		Print(L"Using Font with Size %d\n\r", newFont->psf1_Header->charSize);
+	}
+	else
+	{
+		Print(L"The font is not valid or is missing.\n\r");
+	}
+
+	FrameBuffer* newBuffer = InitializeGOP();
+
+	Print(L"Base: %x\n\rSize: %x\n\rWidth: %d\n\rheight: %d\n\rPixels Per Scanline: %d\n\r", newBuffer->BaseAddress,
+	newBuffer->BufferSize,
+	newBuffer->Width,
+	newBuffer->Height,
+	newBuffer->PixelsPerScanLine);
+
+	Print(L"%s\n\r",KernelStart(newBuffer, newFont));
+
+	return EFI_SUCCESS;
+}
